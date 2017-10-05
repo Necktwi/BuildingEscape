4b633c209449bb563baaa3adb722ed81893fca0a
TMEIC\Gowtham.Kudupudi <gowtham.kudupudi@tmeic.in>
2017-10-05 12:17:47 +0530

    Moving objects in C++ and Laying Out Geometry

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
-
+#include "OpenDoor.h"
+#include <GameFramework/Actor.h>
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,9 +15,16 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Find the owning Actor
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+
+	Owner->SetActorRotation(NewRotation);
 
 	// ...
 	
@@ -25,7 +32,7 @@ void UPositionReport::BeginPlay()
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
