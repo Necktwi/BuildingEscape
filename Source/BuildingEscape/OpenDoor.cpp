1db8eb12eaf010ff3717900f35c30fbed9b35658
TMEIC\Gowtham.Kudupudi <gowtham.kudupudi@tmeic.in>
2017-10-05 14:01:28 +0530

    68. Applying Materials

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
+#include "BuildingEscape.h"
 #include <GameFramework/Actor.h>
 
 // Sets default values for this component's properties
@@ -8,6 +9,7 @@ UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
